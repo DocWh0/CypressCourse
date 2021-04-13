8c75deefb443944d9d7a319777dc62134a2bf647
DocWh0 <77495278+DocWh0@users.noreply.github.com>
2021-04-13 18:44:22 +0300

    Lesson number 19

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,48 +1,112 @@
 ///<reference types="Cypress"/>
 
-it('should', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('[data-qa-node = "amount"]')
-        .type(100)
-        .should('have.value', 100)
-        .and('be.visible')
+
+
+
+//type
+it('type', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-node="phone-number"]') // what method we found and his value 
+        .type(112233344) // value
+})
+
+
+//focus
+it('focus', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-node="amount"]') // what method we found and his value 
+        .focus()
+})
+
+//blur - подкрашивание полей с ошибками
+it('blur', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-node="amount"]') // what method we found and his value 
+        .focus()
+        .blur()
+})
+
+//clear - очиста значения
+it('blur', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-node="amount"]') // what method we found and his value 
+        .type(999)
+        .wait(2000)
+        .clear() 
+})
+
+//submit
+it('Submit', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('form[method="post"]') // what attr we found and his method and value 
+        .submit() // подсветятся не заполненные поля
+})
+
+//click
+it('click', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-value="manual"]') // what attr we found and his method and value 
+        .click() // open form autorization
+})
+
+//rightclick
+it('rightclick', ()=> {
+    cy.visit('https://example.cypress.io/commands/actions') //site
+        .contains('Right click to edit') // what attr we found and his method and value 
+        .rightclick() // open form autorization
+})
+
+
+//doubleclick
+it('doubleclick', ()=> {
+    cy.visit('https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Element/dblclick_event/_samples_/Examples') //site
+        .contains('My Card') // what attr we found and his method and value 
+        .dblclick() // open form autorization
+})
+
+
+//check
+it('check', ()=> {
+    cy.visit('https://www.facebook.com/reg/?') //site
+        .get('input[value="2"]') // what attr we found and his method and value 
+        .check() // open form autorization
+})
+
+//checkbox - штука для переключения тем на привате24
+it('checkbox', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[id*="switchCheckbox"]') /* если ID меняется можно вместо поиска через # использовать 
+                                стандартный поиск и указать id* для поиска по частичному совпадению */
+        .check({force: true})
+        .wait(2000)
+        .uncheck({force: true}) // open form autorization
+})
+
+//select
+it('select', ()=> {
+    cy.visit('https://www.facebook.com/reg/?') //site
+        .get('#month') // 
+        .select('січ') // 
+        .get('#day') // 
+        .select('15') //
+        .get('#year') // 
+        .select('1991') //
+})
+
+
+//select
+it('scrollIntoView', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
+        .get('[data-qa-node="lang"]') // 
+        .wait(2000)
+        .scrollIntoView()
 
 })
 
 
-it('expect', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('[data-qa-node = "amount"]')
-        .type(100).then( input => {
-            expect(input).to.have.value(100)
-        })
-})
-
-
-it('check default value for deposit', () => {
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.get('[data-qa-value="UAH"]')
-        .should('be.checked')
-})
-
-it('check menu, hover myDeposit', () => {
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.contains('Мої депозити')
-        .trigger('mouseover')
-        .get('#archiveDeposits')
-        .should('be.visible')
-})
-
-it('check button type ShowCards ', () => {
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.contains('Show cards')
-        .should('have.attr', 'type')
-        .and('match', /button/)
-})
-
-
-it.only('check is correct URL ', () => {
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.url()
-        .should('eq', 'https://next.privat24.ua/?lang=en')
+//select
+it.only('scrollIntoView', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en') //site 
+        .wait(2000)
+        .scrollTo(0, 500)
 })
