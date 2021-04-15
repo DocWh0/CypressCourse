d7e85ebd4afd439e53963cc875fc2e4cd2b34e54
DocWh0 <77495278+DocWh0@users.noreply.github.com>
2021-04-15 17:45:09 +0300

    firstPageObjectTest

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,122 +1,33 @@
 ///<reference types="Cypress"/>
 
-
-
-
-//type
-it('type', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-node="phone-number"]') // what method we found and his value 
-        .type(112233344) // value
-})
-
-
-//focus
-it('focus', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-node="amount"]') // what method we found and his value 
-        .focus()
-})
-
-//blur - подкрашивание полей с ошибками
-it('blur', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-node="amount"]') // what method we found and his value 
-        .focus()
-        .blur()
-})
-
-//clear - очиста значения
-it('blur', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-node="amount"]') // what method we found and his value 
-        .type(999)
-        .wait(2000)
-        .clear() 
-})
-
-//submit
-it('Submit', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('form[method="post"]') // what attr we found and his method and value 
-        .submit() // подсветятся не заполненные поля
-})
-
-//click
-it('click', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-value="manual"]') // what attr we found and his method and value 
-        .click() // open form autorization
-})
-
-//rightclick
-it('rightclick', ()=> {
-    cy.visit('https://example.cypress.io/commands/actions') //site
-        .contains('Right click to edit') // what attr we found and his method and value 
-        .rightclick() // open form autorization
-})
-
-
-//doubleclick
-it('doubleclick', ()=> {
-    cy.visit('https://yari-demos.prod.mdn.mozit.cloud/en-US/docs/Web/API/Element/dblclick_event/_samples_/Examples') //site
-        .contains('My Card') // what attr we found and his method and value 
-        .dblclick() // open form autorization
-})
-
-
-//check
-it('check', ()=> {
-    cy.visit('https://www.facebook.com/reg/?') //site
-        .get('input[value="2"]') // what attr we found and his method and value 
-        .check() // open form autorization
-})
-
-//checkbox - штука для переключения тем на привате24
-it('checkbox', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[id*="switchCheckbox"]') /* если ID меняется можно вместо поиска через # использовать 
-                                стандартный поиск и указать id* для поиска по частичному совпадению */
-        .check({force: true})
-        .wait(2000)
-        .uncheck({force: true}) // open form autorization
-})
-
-//select
-it('select', ()=> {
-    cy.visit('https://www.facebook.com/reg/?') //site
-        .get('#month') // 
-        .select('січ') // 
-        .get('#day') // 
-        .select('15') //
-        .get('#year') // 
-        .select('1991') //
-})
-
-
-//scrollIntoView
-it('scrollIntoView', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site
-        .get('[data-qa-node="lang"]') // 
-        .wait(2000)
-        .scrollIntoView()
+it('Replenishment of Ukraine mobile phone number', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="phone-number"]')
+        .type('930358540') //check mobile number for error
+        .get('[data-qa-node="amount"]')
+        .type('50')
+        .get('[data-qa-node="numberdebitSource"]')
+        .type('4552331448138217')
+        .get('[data-qa-node="expiredebitSource"]')
+        .type('0524') //ошибка
+        .get('[data-qa-node="cvvdebitSource"]')
+        .type('111')
+        .wait(3000)
+        .get('[data-qa-node="submit"]')
+        .click()
+        .get('[data-qa-node="details"]')
+        .should('contain.text', '+380930358540')
+        .get('[data-qa-node="card"]')
+        .should('have.text', '4552 **** **** 8217')
+        .get('[data-qa-node="amount"]')
+        .should('have.text', '50')
+        .get('[data-qa-node="currency"]')
+        .eq(0)
+        .should('contain.text', 'UAH')
+        .get('[data-qa-node="commission"]')
+        .eq(1)
+        .should('have.text', '2')
+        .get('[data-qa-node="commission-currency"]')
+        .should('contain.text', 'UAH') 
 
 })
-
-
-//scrollTo
-it('scrollIntoView', ()=> {
-    cy.visit('https://next.privat24.ua/mobile?lang=en') //site 
-        .wait(2000)
-        .scrollTo(0, 500)
-})
-
-//trigger
-it.only('scrollIntoView', ()=> {
-    cy.visit('https://next.privat24.ua?lang=en') //site 
-        .contains('Services')
-        .wait(2000)
-        .trigger('mouseover')
-})
-
-
